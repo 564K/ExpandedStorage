205bed9f2040e19f3bf0a9850d38d9d988d33b0a
NinjaPhenix <5214513+NinjaPhenix@users.noreply.github.com>
2021-05-16 13:52:01 +0100

    Rework buildscript more, transition over common modules.

---
+++ common/src/main/java/ninjaphenix/expandedstorage/base/internal_api/item/ItemUpgradeBehaviour.java
@@ -0,0 +1,15 @@
+package ninjaphenix.expandedstorage.base.internal_api.item;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.ApiStatus.Experimental;
+import org.jetbrains.annotations.ApiStatus.Internal;
+
+@Experimental
+@Internal
+public interface ItemUpgradeBehaviour {
+    // todo: remove level param?
+    boolean tryUpgradeItem(Level level, Player player, ItemStack upgrade, ItemStack target, ResourceLocation from, ResourceLocation to);
+}
