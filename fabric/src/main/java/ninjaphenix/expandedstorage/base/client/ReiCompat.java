3a0ccc811a57af4d91f0d24d657d3403b17bfca8
NinjaPhenix <5214513+NinjaPhenix@users.noreply.github.com>
2021-06-03 11:01:16 +0100

    Update to REI 6 api & MC pre 4

--- fabric/src/main/java/ninjaphenix/expandedstorage/base/client/ReiCompat.java
+++ fabric/src/main/java/ninjaphenix/expandedstorage/base/client/ReiCompat.java
@@ -1,36 +1,19 @@
 package ninjaphenix.expandedstorage.base.client;
 
 import me.shedaniel.math.Rectangle;
-import me.shedaniel.rei.api.BaseBoundsHandler;
-import me.shedaniel.rei.api.DisplayHelper;
-import me.shedaniel.rei.api.plugins.REIPluginV0;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screens.Screen;
+import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
+import me.shedaniel.rei.api.client.registry.screen.ExclusionZones;
+import me.shedaniel.rei.api.common.util.CollectionUtils;
 import net.minecraft.client.renderer.Rect2i;
-import net.minecraft.resources.ResourceLocation;
 import ninjaphenix.expandedstorage.base.client.menu.AbstractScreen;
-import ninjaphenix.expandedstorage.base.internal_api.Utils;
 
-import java.util.Collections;
-import java.util.stream.Collectors;
-
-public class ReiCompat implements REIPluginV0 {
+public class ReiCompat implements REIClientPlugin {
     private static Rectangle toReiRect(Rect2i rect) {
         return new Rectangle(rect.getX(), rect.getY(), rect.getWidth(), rect.getHeight());
     }
 
     @Override
-    public ResourceLocation getPluginIdentifier() {
-        return Utils.resloc("rei_plugin");
-    }
-
-    @Override
-    public void registerBounds(DisplayHelper displayHelper) {
-        BaseBoundsHandler.getInstance().registerExclusionZones(AbstractScreen.class, () -> {
-            if (Minecraft.getInstance().screen instanceof AbstractScreen<?, ?> screen) {
-                return screen.getExclusionZones().stream().map(ReiCompat::toReiRect).collect(Collectors.toList());
-            }
-            return Collections.emptyList();
-        });
+    public void registerExclusionZones(ExclusionZones zones) {
+        zones.register(AbstractScreen.class, (AbstractScreen<?, ?> screen) -> CollectionUtils.map(screen.getExclusionZones(), ReiCompat::toReiRect));
     }
 }
